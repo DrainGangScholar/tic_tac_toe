234141a8360ac40b9410ff2ae83c199c0f82ea13
DrainGangScholar <veljko.markovic@elfak.rs>
2023-05-21 02:26:17 +0200

    Sto vise radim to manje znam i to manje radi :DDD

--- src/main.rs
+++ src/main.rs
@@ -5,22 +5,18 @@ const COLS: usize = 3;
 const X_WIN: [char; 3] = ['X', 'X', 'X'];
 const O_WIN: [char; 3] = ['O', 'O', 'O'];
 fn main() {
-    // let mut board = Board::empty();
-    // println!("{}", &board);
-    // loop {
-    //     match board.make_move(1, 2, 'X') {
-    //         Ok(_) => println!("{}", &board),
-    //         Err(_) => continue,
-    //     }
-    // }
-    let mut board = Board::default();
-    loop {
-        //treba da bude neka alternacija izmedju algoritma i pravog igraca D::D
-        match board.make_move(7, 4, 'X') {
-            Ok(_) => println!("{}", &board),
-            Err(err) => println!("{}", err)(),
-        };
-    }
+    let mut board = Board::empty();
+    match board.make_move(1, 2, 'X') {
+        Ok(_) => println!("{}", &board),
+        Err(_) => println!("MRs"),
+    };
+    let mut alpha = i32::MIN;
+    let mut beta = i32::MAX;
+    board.alpha_beta(9, &mut alpha, &mut beta, false);
+    match board.make_move(alpha as usize, beta as usize, '0') {
+        Ok(_) => println!("{}", &board),
+        Err(_) => println!("MRs"),
+    };
 }
 
 struct Board {
@@ -43,7 +39,54 @@ impl Board {
         self.board[row][col] = choice;
         Ok(())
     }
-    fn _evaluate(&mut self) -> i32 {
+    fn alpha_beta(
+        &mut self,
+        depth: i32,
+        alpha: &mut i32,
+        beta: &mut i32,
+        is_maximizing_player: bool,
+    ) -> Result<(), i32> {
+        if depth == 0 || self.evaluate() != 0 {
+            return self.evaluate();
+        }
+        if is_maximizing_player {
+            let mut value = std::i32::MIN;
+            for (row, col) in self.generate_moves() {
+                self.make_move(row, col, 'X').unwrap();
+                value = value.min(self.alpha_beta(depth - 1, alpha, beta, false));
+                self.board[row][col] = ' '; //undo
+                *alpha = *(alpha).max(&mut value);
+                if alpha >= beta {
+                    break;
+                }
+            }
+            value
+        } else {
+            let mut value = std::i32::MAX;
+            for (row, col) in self.generate_moves() {
+                self.make_move(row, col, 'O').unwrap();
+                value = value.max(self.alpha_beta(depth - 1, alpha, beta, true));
+                self.board[row][col] = ' '; //undo
+                *beta = *(beta).min(&mut value);
+                if alpha >= beta {
+                    break;
+                }
+            }
+            value
+        }
+    }
+    fn generate_moves(&mut self) -> Vec<(usize, usize)> {
+        let mut moves = Vec::new();
+        for i in 0..ROWS {
+            for j in 0..COLS {
+                if self.board[i][j] == ' ' {
+                    moves.push((i, j));
+                }
+            }
+        }
+        moves
+    }
+    fn evaluate(&mut self) -> i32 {
         // 1 x pobednik, -1 o pobednik, 0 nereseno/nema pobednika jos?? treba i o tome da se razmislja...
         for i in 0..ROWS {
             match self.board[i] {
@@ -81,12 +124,13 @@ impl Board {
         }
         0
     }
-}
-impl Default for Board {
-    fn default() -> Self {
-        Board {
-            board: [['X', 'O', 'X'], ['O', 'X', 'O'], ['O', 'X', 'O']],
-        }
+    fn is_board_full(&mut self) -> bool {
+        self.board
+            .iter()
+            .all(|row| row.iter().all(|&cell| cell != ' '))
+    }
+    fn is_game_over(&mut self) -> bool {
+        self.evaluate() != 0 || self.is_board_full()
     }
 }
 impl Display for Board {
